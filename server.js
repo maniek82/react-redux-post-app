a6e5fa7de6ce37fb5dac34a9568dd56bd2063b7e
Mariusz <mariusz.tripler@gmail.com>
2016-11-25 20:57:45 +0000

    Changed app.get('/')

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const port = process.env.PORT || 8080;
-var serveStatic = require('serve-static');
+
 const app = express();
 
-app.use(ser(__dirname));
+app.use(express.static(__dirname));
 
 
-app.get('*', (req,res)=> {
+app.get('/', (req,res)=> {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
