99ac3446438e2291a83e05034d4b691157d867f6
Mariusz <mariusz.tripler@gmail.com>
2016-11-25 20:06:12 +0000

    More changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import {Router, browserHistory} from 'react-router';
 import reducers from './reducers';
-import routes from './routes';
+import routes from './routes'; 
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
